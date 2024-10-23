fc67aca90c7937cbcc48e4ba02ea1a752a59ac90
Abdulla-Khan <abdullahkhan.smiu@gmail.com>
2024-10-23 16:57:53 +0500

    App Completed

---
+++ Appetizers/Views/Screens/AccountsView/AccountViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AccountViewModel.swift
+//  Appetizers
+//
+//  Created by Ayaz on 23/10/2024.
+//
+
+import SwiftUI
+
+struct AccountViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountViewModel()
+}
